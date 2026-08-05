1a94aa57b4a33dabbbd3deaec08179b750248189
Manish Kumar <mankan2105@gmail.com>
2026-08-05 11:22:28 +0530

    the app file where componnests of apps are mounted is added

---
+++ React/module_5/useEffectHook/useEffectTuto/tuto_1/src/App.jsx
@@ -0,0 +1,54 @@
+//import OrderA from './components/OrderA.jsx'  ;
+//import OrderB from './components/OrderB.jsx';
+
+//import OrderC from "./components/OrderC";
+
+// import OrderD from "./components/OrderD.jsx" ;
+// import OrderE from "./components/OrderE.jsx";
+
+// import "./App.css"
+
+// import OrderF from "./components/OrderF";
+
+ import StopWatch from "./components/StopWatch";
+ 
+ 
+
+//import OrderG from "./components/OrderG"
+// import Timer from "./components/Timer"
+
+
+
+
+const App = () => {
+
+
+
+  return (
+    <>
+      {/* <OrderA/> */}
+
+      {/* <OrderB/> */}
+
+      {/* <OrderC/> */}
+      
+       {/* <OrderD/> */}
+
+       {/* <OrderE/> */}
+
+       {/* <OrderF/> */}
+       {/* <OrderG/> */}
+
+       <StopWatch/>
+     
+       {/* <Timer/> */}
+
+       
+     
+
+      
+    </>
+  )
+}
+
+export default App
